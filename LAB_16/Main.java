1e543f3397afe5ee4919537c731d386bcd627cb6
Xen0n-n <71091235+Xen0n-n@users.noreply.github.com>
2020-12-17 14:12:13 +0300

    Update Main.java

--- LAB_16/Main.java
+++ LAB_16/Main.java
@@ -3,9 +3,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> lst = new CycleList<>();
-        lst.add("Jopa");
-        lst.add("Her");
-        lst.add("Mocha");
+        lst.add("block21");
+        lst.add("block2");
+        lst.add("block1");
 
         System.out.println(lst.get(1));
         System.out.println(lst.size());
