b7850c609f7fd6c0b68cd42bb9487d8fb58a4b5d
Xen0n-n <71091235+Xen0n-n@users.noreply.github.com>
2020-12-17 14:14:21 +0300

    Update Main.java

--- Pract16/Main.java
+++ Pract16/Main.java
@@ -3,9 +3,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> lst = new CycleList<>();
-        lst.add("Jopa");
-        lst.add("Her");
-        lst.add("Mocha");
+        lst.add("first");
+        lst.add("second");
+        lst.add("third");
 
         System.out.println(lst.get(1));
         System.out.println(lst.size());
