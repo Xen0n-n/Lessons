530c16ad08fcd5f0af058280ddd02246c8c40f0e
Xen0n-n <71091235+Xen0n-n@users.noreply.github.com>
2020-12-17 13:58:11 +0300

    Delete Main.java

--- Lab10/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static <E> E get(E[] arr, int index) {
-        return arr[index];
-    }  
-    public static void main(String[] args) {
-        String[] words = {"apple", "google", "Huawei", "Xiaomi"};
-        System.out.println(get(i, 2));
-    }
-}
